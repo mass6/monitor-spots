b249f7843e10711f47c5da37c8c33fadc0f5da3a
Sam Ciaramilaro <sc@kontainer.com>
2023-08-01 20:01:11 +0200

    Add route to check via web

---
+++ app/Http/Controllers/CheckSpotsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Redis;
+use Symfony\Component\DomCrawler\Crawler;
+
+class CheckSpotsController extends Controller
+{
+    public function __invoke()
+    {
+        $spotsAvailable = $this->getSpotsAvailable() ?? 'Error';
+        return response()->json(['message' => $spotsAvailable])
+            ->header('Content-Type', 'application/json')
+            ->header('Access-Control-Allow-Origin', '*');
+    }
+
+    private function getSpotsAvailable()
+    {
+        $url = 'https://ultrasignup.com/register.aspx?did=102258';
+        $client = new Client();
+        try {
+            $response = $client->get($url);
+            $html = (string) $response->getBody();
+            $crawler = new Crawler($html);
+            $spotsElement = $crawler->filter("span#ContentPlaceHolder1_PriceList_lblSpots_0");
+            if ($spotsElement->count() > 0) {
+                return trim($spotsElement->text());
+            }
+            return null;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+}
